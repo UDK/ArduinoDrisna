16a6b1f277c5df3e20eb2573fa5c1d492519cc23
batyrgali1 <38011821+batyrgali1@users.noreply.github.com>
2018-04-03 22:22:49 +0300

    Update trevoga.ino

--- trevoga.ino
+++ trevoga.ino
@@ -15,7 +15,7 @@ void setup()
 void loop()
 {
     boolean currBtn = digitalRead(btnPin);
-    if (currBtn==1) {
+    if (currBtn==true) {
         //сразу после нажатия кнопки отправляем "тревожное" сообщение по заданному номеру
         sendTextMessage();
     }
